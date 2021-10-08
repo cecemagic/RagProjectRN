3477b0e64bc1347d98e3c646b1025a09e4010177
Chau Cottrell <chauchac98@gmail.com>
2021-10-08 15:33:36 -0700

    HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -5,27 +5,38 @@ import {
   View,
   TextInput,
   ImageBackground,
+  Dimensions,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import homeScreenImage from "../../assets/homeScreen.jpeg";
+import titleFont from "../../assets/Marshland_Beauty.otf";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.parentCard}>
-            <ImageBackground
-              source={homeScreenImage}
-              resizeMode="cover"
-              style={styles.image}
-            >
-              <View style={styles.boxText}>
-                <Text style={styles.text}>Rent A Guy</Text>
-              </View>
-            </ImageBackground>
+        <ImageBackground
+          source={homeScreenImage}
+          resizeMode="cover"
+          style={styles.image}
+        >
+          <View>
+            <Text style={styles.titleText}>Rent A Guy</Text>
           </View>
+          <View style={styles.cardOptions}>
+            <Text style={styles.cardText}>Rent A Guy</Text>
+          </View>
+          <View style={styles.cardOptions}>
+            <Text style={styles.cardText}>Be a Date</Text>
+          </View>
+        </ImageBackground>
+      </View>
     </SafeAreaView>
   );
 }
+
+const { width: viewportWidth, height: viewportHeight } =
+  Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
@@ -33,27 +44,38 @@ const styles = StyleSheet.create({
   },
   parentCard: {
     flex: 1,
-    backgroundColor: 'pink',
+    backgroundColor: "pink",
   },
-//   card: {
-//     flex: 1,
-//     backgroundColor: 'pink',
-//   },
   image: {
     flex: 1,
+    height: viewportHeight,
+    width: viewportWidth,
   },
-  boxText: {
-    backgroundColor: 'green',
-    opacity: 0.6,
-  },
-  text: {
-      position: 'absolute',
-    backgroundColor: 'pink',
+  titleText: {
     alignSelf: "center",
-    color: "white",
-    marginTop: 350,
+    color: "#639A66",
+    marginTop: 300,
     marginLeft: 9,
     fontWeight: "bold",
-    fontSize: 25,
+    fontSize: 45,
+    textShadowColor: "black",
+    textShadowOffset: { width: -2, height: 2 },
+    textShadowRadius: 15,
+  },
+  cardOptions: {
+    backgroundColor: "rgba(188, 179, 130, 0.5)",
+    borderRadius: 50,
+    marginHorizontal: 80,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingHorizontal: 100, 
+  },
+  cardText: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "white",
+    textShadowColor: "black",
+    textShadowOffset: { width: -2, height: 2 },
+    textShadowRadius: 15,
   },
 });
